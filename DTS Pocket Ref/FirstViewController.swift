21c1f9f4e54db8bde53dbfd17894a6900681710f
dtsuk <Darren@openvpn-client.vpnnc.dtssoftware.com>
2016-06-30 20:36:11 +0100

    change to SSL

--- DTS Pocket Ref/FirstViewController.swift
+++ DTS Pocket Ref/FirstViewController.swift
@@ -16,7 +16,7 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let URL = NSURL(string: "http://www.dtssoftware.com/company_aboutdts.htm")
+        let URL = NSURL(string: "https://www.dtssoftware.com/company_aboutdts.htm")
         
         webView.loadRequest(NSURLRequest(URL: URL!))
         
